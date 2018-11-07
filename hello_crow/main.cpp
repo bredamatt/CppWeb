dd38811360659ba563cd917ed517aa09439de28b
Mattia Louis Vik Bradascio <mattia@Mattias-MacBook-Pro.local>
2018-11-07 20:32:15 +0100

    added welcome

--- hello_crow/main.cpp
+++ hello_crow/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[])
 
   CROW_ROUTE(app, "/")
     ([](){
-      return "<div><h1>Hello, Mattia.</h1></div>";
+      return "<div><h1>Hello, Mattia. Welcome! </h1></div>";
     });
 
   char* port = getenv("PORT");
