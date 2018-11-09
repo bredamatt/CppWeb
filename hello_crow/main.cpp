8686ad1ef50e7a52719d354e5fcd1eb05fbb5164
Mattia Louis Vik Bradascio <mattia@Mattias-MacBook-Pro.local>
2018-11-09 13:46:33 +0100

    changed getView to return void, added parameters to route handlers

--- hello_crow/main.cpp
+++ hello_crow/main.cpp
@@ -30,8 +30,11 @@ using namespace std;
 using namespace crow;
 using namespace crow::mustache;
 
-string getView(const string &filename, context &x) {
-  return load("../public/" + filename + ".html").render(x);
+void getView(response &res, const string &filename, context &x) {
+  res.set_header("Content-Type", "text/html");
+  auto text = load("../public/" + filename + ".html").render(x);
+  res.write(text);
+  res.end();
 }
 
 // Route handler helper functions
@@ -67,6 +70,11 @@ void sendStyle(response &res, string filename){
   sendFile(res, "styles/" + filename, "text/css");
 }
 
+void notFound(response &res, const string &message){
+  res.code=404;
+  res.write(message + ": Not Found");
+  res.end();
+}
 
 int main(int argc, char* argv[]){
   crow::SimpleApp app;
@@ -93,15 +101,28 @@ int main(int argc, char* argv[]){
     });
 
   CROW_ROUTE(app, "/contact/<string>")
-  ([&collection](string email){
+  ([&collection](const request &req, response &res, string email){
     auto doc = collection.find_one(make_document(kvp("email", email)));
     crow::json::wvalue dto; // data transfer object
     dto["contact"] = json::load(bsoncxx::to_json(doc.value().view()));
-    return getView("contact", dto);
+    getView(res, "contact", dto);
+  });
+
+  CROW_ROUTE(app, "/contact/<string>/<string>")
+  ([&collection](const request &req, response &res, string firstname,
+    string lastname){
+    auto doc = collection.find_one(make_document(kvp("firstName", firstname), kvp("lastName", lastname)));
+    if(!doc) {
+      return notFound(res, "Contact");
+    }
+
+    crow::json::wvalue dto; // data transfer object
+    dto["contact"] = json::load(bsoncxx::to_json(doc.value().view()));
+    getView(res, "contact", dto);
   });
 
   CROW_ROUTE(app, "/contacts")
-    ([&collection](){
+    ([&collection](const request &req, response &res){
       mongocxx::options::find opts;
       opts.skip(9);
       opts.limit(10);
@@ -114,11 +135,11 @@ int main(int argc, char* argv[]){
         contacts.push_back(json::load(bsoncxx::to_json(doc)));
       }
       dto["contacts"] = contacts;
-      return getView("contacts", dto);
+      getView(res, "contacts", dto);
     });
 
 
-  CROW_ROUTE(app, "/rest_test").methods(HTTPMethod::Post) // implementation of Post
+  CROW_ROUTE(app, "/rest_test").methods(HTTPMethod::Post, HTTPMethod::Put, HTTPMethod::Get) // implementation of Post
     ([](const request &req, response &res){
       string method = method_name(req.method);
       res.set_header("Content-Type", "text/plain");
